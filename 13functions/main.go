f3eab5c7a07f67810d6411f0ddecd292d78ef077
Fazal <fazalbkabeer@gmail.com>
2025-10-31 22:21:17 +0530

    CLI fn that returns sum,product,quotient,difference

--- 13functions/main.go
+++ 13functions/main.go
@@ -1,4 +1,20 @@
 package main
-func main()  {
-	
+
+import "fmt"
+
+func calculation(num1, num2 float64) (sum, product, quo, diff float64) {
+	sum = num1 + num2
+	product = num1 * num2
+	quo = num1 / num2
+	diff = num1 - num2
+
+	return sum, product, quo, diff
+}
+func main() {
+	a := 5.0
+	b := 10.0
+
+	s, p, q, d := calculation(a, b)
+
+	fmt.Println(":", s, p, q, d)
 }
