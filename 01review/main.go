a8d88849242b58679def7d21003cafc87db9dd0a
Fazal <fazalbkabeer@gmail.com>
2025-11-04 14:55:34 +0530

    learned concurrency, goroutines, chanels

--- 01review/main.go
+++ 01review/main.go
@@ -2,50 +2,52 @@ package main
 
 import "fmt"
 
-func divide(n1 int,n2 int) int {
+func divide(n1 int, n2 int) int {
 	if n2 == 0 {
 		panic("Cant divide by zero")
 	}
-	return n1/n2
+	return n1 / n2
 }
 
-func main()  {
-	defer func ()  {
-		if r := recover(); r != nil{
-			fmt.Println("Panic Recovered:",r)
-		}	
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Panic Recovered:", r)
+		}
 	}()
 
-	fmt.Println(divide(50,0))
+	fmt.Println(divide(48, 5))
+	fmt.Println(divide(50, 0))
+
 }
 
-	// op := ""
+//num1,num2 := 0
+// op := ""
 
-	// fmt.Println("Enter first operator")
-	// fmt.Scan(&num1)
+// fmt.Println("Enter first operator")
+// fmt.Scan(&num1)
 
-	// fmt.Println("Enter Operation (+,-,*,/)")
-	// fmt.Scan(&op)
+// fmt.Println("Enter Operation (+,-,*,/)")
+// fmt.Scan(&op)
 
-	// fmt.Println("Enter second operator")
-	// fmt.Scan(&num2)
+// fmt.Println("Enter second operator")
+// fmt.Scan(&num2)
 
-	// result := 0
+// result := 0
 
-	// switch op {
-	// case "+":
-	// 	result = num1 + num2
-	// case "-":
-	// 	result = num1 - num2
-	// case "*":
-	// 	result = num1 * num2
-	// case "/":
-	// 	if num2 == 0 {
-	// 		fmt.Println("Error! division by zero")
-	// 		return
-	// 	}
-	// 	result = num1 / num2
-	// }
+// switch op {
+// case "+":
+// 	result = num1 + num2
+// case "-":
+// 	result = num1 - num2
+// case "*":
+// 	result = num1 * num2
+// case "/":
+// 	if num2 == 0 {
+// 		fmt.Println("Error! division by zero")
+// 		return
+// 	}
+// 	result = num1 / num2
+// }
 
-	// fmt.Println("Result:",result)
-
+// fmt.Println("Result:",result)
