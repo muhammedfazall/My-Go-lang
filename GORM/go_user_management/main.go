1e845e00760de5aac04be383add09079e6d7a73f
Fazal <fazalbkabeer@gmail.com>
2025-11-30 23:40:30 +0530

    GORM & gin

--- GORM/go_user_management/main.go
+++ GORM/go_user_management/main.go
@@ -2,10 +2,10 @@ package main
 
 func main()  {
 	
-	type User struct {
-    ID    uint   `gorm:"primaryKey" json:"id"`
-    Name  string `json:"name"`
-    Email string `gorm:"unique" json:"email"`
-    Age   int    `json:"age"`
-}
+// 	type User struct {
+//     ID    uint   `gorm:"primaryKey" json:"id"`
+//     Name  string `json:"name"`
+//     Email string `gorm:"unique" json:"email"`
+//     Age   int    `json:"age"`
+// }
 }
